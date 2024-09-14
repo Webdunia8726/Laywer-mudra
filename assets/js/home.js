54fdc4550d03ca7bb3f06097301b597b658e238d
raj patel <rajpatel8726@gmail.com>
2024-09-14 13:47:37 +0530

    add

---
+++ assets/js/home.js
@@ -0,0 +1,16 @@
+  // Fade-in Animation Script
+  document.addEventListener('DOMContentLoaded', function () {
+    const fadeElements = document.querySelectorAll('.fade-in');
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    });
+
+    fadeElements.forEach(el => observer.observe(el));
+  });
+
+
+  
